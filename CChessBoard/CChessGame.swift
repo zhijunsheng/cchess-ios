712aa94769cba329685d58f61411936f557e07dd
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-13 16:08:50 -0500

    deployed all 32 pieces in CChessGame

--- CChessBoard/CChessGame.swift
+++ CChessBoard/CChessGame.swift
@@ -12,13 +12,29 @@ struct CChessGame {
     
     mutating func initBoard() {
         for i in 0..<2 {
-            piecesBox.insert(CChessPiece(col: 0 + 8 * i, row: 9, player: .black, rank: .rook, imageName: "bj"))
-            piecesBox.insert(CChessPiece(col: 0 + 8 * i, row: 0, player: .black, rank: .rook, imageName: "rj"))
+            piecesBox.insert(CChessPiece(col: 0 + i * 8, row: 9, player: .black, rank: .rook, imageName: "bj"))
+            piecesBox.insert(CChessPiece(col: 0 + i * 8, row: 0, player: .black, rank: .rook, imageName: "rj"))
             
-            piecesBox.insert(CChessPiece(col: 1 + 6 * i, row: 9, player: .black, rank: .rook, imageName: "bm"))
-            piecesBox.insert(CChessPiece(col: 1 + 6 * i, row: 0, player: .black, rank: .rook, imageName: "rm"))
+            piecesBox.insert(CChessPiece(col: 1 + i * 6, row: 9, player: .black, rank: .knight, imageName: "bm"))
+            piecesBox.insert(CChessPiece(col: 1 + i * 6, row: 0, player: .black, rank: .knight, imageName: "rm"))
+            
+            piecesBox.insert(CChessPiece(col: 2 + i * 4, row: 9, player: .black, rank: .bishop, imageName: "bx"))
+            piecesBox.insert(CChessPiece(col: 2 + i * 4, row: 0, player: .black, rank: .bishop, imageName: "rx"))
+            
+            piecesBox.insert(CChessPiece(col: 3 + i * 2, row: 9, player: .black, rank: .warrior, imageName: "bs"))
+            piecesBox.insert(CChessPiece(col: 3 + i * 2, row: 0, player: .black, rank: .warrior, imageName: "rs"))
+            
+            piecesBox.insert(CChessPiece(col: 1 + i * 6, row: 7, player: .black, rank: .cannon, imageName: "bp"))
+            piecesBox.insert(CChessPiece(col: 1 + i * 6, row: 2, player: .black, rank: .cannon, imageName: "rp"))
         }
         
+        for i in 0..<5 {
+            piecesBox.insert(CChessPiece(col: i * 2, row: 6, player: .black, rank: .pawn, imageName: "bz"))
+            piecesBox.insert(CChessPiece(col: i * 2, row: 3, player: .black, rank: .pawn, imageName: "rz"))
+        }
+        
+        piecesBox.insert(CChessPiece(col: 4, row: 9, player: .black, rank: .king, imageName: "bb"))
+        piecesBox.insert(CChessPiece(col: 4, row: 0, player: .black, rank: .king, imageName: "rb"))
     }
     
 }
