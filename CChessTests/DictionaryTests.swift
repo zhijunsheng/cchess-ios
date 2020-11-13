11090fc0641af5abc19693efec0cefd275bcf544
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-13 17:37:07 -0500

    created Dictionary<CChessPiece, UIImage>

--- CChessTests/DictionaryTests.swift
+++ CChessTests/DictionaryTests.swift
@@ -6,8 +6,21 @@
 //
 
 import XCTest
+@testable import CChessBoard
 
 class DictionaryTests: XCTestCase {
+    
+    func testCChessPieceImageDictionary() {
+        var keyCChessPieceValueUIImage: Dictionary<CChessPiece, UIImage> = [:]
+        let leftBlackRook = CChessPiece(col: 0, row: 9, player: .black, rank: .rook, imageName: "bj")
+        let leftBlackRookImage: UIImage? = UIImage(named: leftBlackRook.imageName)
+        
+        keyCChessPieceValueUIImage[leftBlackRook] = nil
+        XCTAssertEqual(0, keyCChessPieceValueUIImage.count)
+        
+        keyCChessPieceValueUIImage[leftBlackRook] = leftBlackRookImage
+        XCTAssertEqual(1, keyCChessPieceValueUIImage.count)
+    }
 
     func testIntIntDictionary() {
         var keyIntValueInt: Dictionary<Int, Int> = Dictionary<Int, Int>()
@@ -17,5 +30,12 @@ class DictionaryTests: XCTestCase {
         XCTAssertEqual(27, keyIntValueInt[3])
         XCTAssertEqual(2197, keyIntValueInt[13])
     }
-
+    
+    func testStringIntDictionary() {
+        var keyStringValueInt: Dictionary<String, Int> = [:]
+        keyStringValueInt["Golden Thumb"] = 3030303030303031
+        
+        XCTAssertNil(keyStringValueInt["Donald"])
+        XCTAssertEqual(3030303030303031, keyStringValueInt["Golden Thumb"])
+    }
 }
